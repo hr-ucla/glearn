df08cb91edb55ee80cc7f63f7ec6a6a94ac1af66
Jason Hong <jasonhong1240@gmail.com>
2020-11-03 17:53:56 -0800

    finished the summary page for the quiz

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React, {useState} from 'react';
 import { View } from 'react-native';
 import SignIn from './SignIn.js';
 import Deliverables from './Deliverables.js';
+import QuizDescription from './QuizDescription.js';
+import QuizContent from './QuizContent.js';
 
 export default function App() {
   return (
     <>
-      <SignIn />
-      <Deliverables />
+      <QuizDescription />
     </>
   );
 }
