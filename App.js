f510f330e3f4e752377e7ab50a9840fa715ccd93
Jason Hong <jasonhong1240@gmail.com>
2020-11-07 15:29:53 -0800

    fixed some styling

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import Home from './pages/Home.js';
 import LoginScreen from './screens/LoginScreen.js';
 import LoadingScreen from './screens/LoadingScreen.js';
 
+console.ignoredYellowBox = ['Warning: Each', 'Warning: Failed']
+
 firebase.initializeApp(firebaseConfig);
 
 const AppSwitchNavigator = createSwitchNavigator(
