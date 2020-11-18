c2fecbadf7b65e196d42dc1ecb6579c1c4ac787f
timkang93 <jeongmo.kang@gmail.com>
2020-11-17 20:25:27 -0800

    [ops] add config for firebase

---
+++ config/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: “AIzaSyAm35EWWSTlRVN5bXnx74I7mwKnqmeAIJY”,
+  authDomain: “glearn-459d7.firebaseapp.com”,
+  databaseURL: “https://glearn-459d7.firebaseio.com”,
+  projectId: “glearn-459d7",
+  storageBucket: “glearn-459d7.appspot.com”,
+  messagingSenderId: “309263535474",
+  appId: “1:309263535474:web:2cf50f63b0f287728b21eb”,
+  measurementId: “G-GE6ZB9WL2L”
+};
